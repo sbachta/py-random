9ef5e00490f7bea862d214432765a1db2d64ef16
Steve Bachta <sbachta90@gmail.com>
2025-01-18 17:56:50 -0500

    Messing with stuff

--- Scripts.py
+++ Scripts.py
@@ -1,1 +1,10 @@
-print('hello world')
+import requests
+from bs4 import BeautifulSoup
+
+response = requests.get('https://reddit.com/r/StarWars')
+soup = BeautifulSoup(response.content, 'html.parser')
+
+allPosts = soup.find_all('shreddit-post')
+
+for post in allPosts:
+    print(post.get('post-title'), post.get('content-href'))
